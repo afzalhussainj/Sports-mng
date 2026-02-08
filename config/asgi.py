d2ae0b1b7910c8eadfcc9765ed606892726a603c
Afzal Hussain <afzall.hj@gmail.com>
2026-02-08 15:03:58 +0500

    Refactor ASGI configuration: remove unnecessary django import and encapsulate WebSocket application logic

--- config/asgi.py
+++ config/asgi.py
@@ -3,7 +3,6 @@ ASGI config for sports_gala project.
 """
 
 import os
-import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -13,21 +12,27 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODUL
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
-django.setup()
 django_asgi_app = get_asgi_application()
 
-from core import routing as core_routing
+
+class LazyWebSocketApplication:
+    def __init__(self):
+        self._app = None
+
+    async def __call__(self, scope, receive, send):
+        if self._app is None:
+            from core import routing as core_routing
+            self._app = AllowedHostsOriginValidator(
+                AuthMiddlewareStack(
+                    URLRouter(core_routing.websocket_urlpatterns)
+                )
+            )
+        return await self._app(scope, receive, send)
 
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
     "http": django_asgi_app,
 
     # WebSocket chat handler with authentication
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                core_routing.websocket_urlpatterns
-            )
-        )
-    ),
+    "websocket": LazyWebSocketApplication(),
 })
