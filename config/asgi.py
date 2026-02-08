9fe3f052f5d911753c40927573725a8972494e9c
Afzal Hussain <afzall.hj@gmail.com>
2026-02-08 14:45:56 +0500

    Reorder import statements in ASGI configuration for clarity

--- config/asgi.py
+++ config/asgi.py
@@ -7,13 +7,14 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-import core.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+import core.routing
 
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
